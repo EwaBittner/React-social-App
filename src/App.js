76b01c836076c951c1704c34431af2e2338af555
Ewa Bittner <bittner.ewa@gmail.com>
2022-01-19 22:56:44 +0100

    Started adding routing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {MyFavouriteDishes} from "./Dishes";
 import Wrapper from "./Wrapper";
 import Users from "./Users";
 import {Form} from "./Form";
+import { Routes, Route} from "react-router-dom";
 
 const dishes = [
   { id: 1, name: 'Bigos' }, 
@@ -24,7 +25,7 @@ function App() {
         <Toolbar>
           <CodeIcon />
           <Button variant="text" color="inherit">Home</Button>
-          <Button variant="text" color="inherit">About us</Button>
+          <Button variant="text" color="inherit">About me</Button>
           <Button variant="text" color="inherit">Game</Button>
           <Button variant="text" color="inherit">Users</Button>
           <Button variant="text" color="inherit">Form</Button>
@@ -33,17 +34,29 @@ function App() {
       </AppBar>
 
       <Wrapper>
-        <MyName name="Ewa" surname="Bit" />
-        <MyName name="Ewa" />
-        <MyFavouriteDishes dishes={dishes} />
-        <Form />
-        <Contact data={{
-          tel: '4646345',
-          address: 'Morenevegen 15',
-          email: 'koteczki@tak.no'
-        }} />
-        <Game name="Szaszki"/>
-        <Users />
+        <Routes>
+          <Route path="/about-me">
+            <MyName name="Ewa" surname="Bit" />
+            <MyName name="Ewa" />
+            <MyFavouriteDishes dishes={dishes} />
+          </Route>
+          <Route path="/game" element={<Game name="Szaszki"/>} >
+            
+          </Route>
+          <Route path="/users" element={<Users />} >
+            
+          </Route>
+          <Route path="/form" element={<Form />} >
+            
+          </Route>
+          <Route path="/contact" element={<Contact data={{
+              tel: '4646345',
+              address: 'Morenevegen 15',
+              email: 'koteczki@tak.no'
+            }} />} >
+            
+          </Route>
+        </Routes>
       </Wrapper>
     </>
   );

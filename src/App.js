cd39afad9267b417c33bfa7eb7f50927df9a8b19
Ewa Bittner <bittner.ewa@gmail.com>
2022-01-20 23:43:43 +0100

    change from switch to routes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {MyFavouriteDishes} from "./Dishes";
 import Wrapper from "./Wrapper";
 import Users from "./Users";
 import {Form} from "./Form";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, BrowserRouter} from "react-router-dom";
 
 const dishes = [
   { id: 1, name: 'Bigos' }, 
@@ -36,26 +36,27 @@ function App() {
       <Wrapper>
         <Routes>
           <Route path="/about-me">
-            <MyName name="Ewa" surname="Bit" />
+            {/* <MyName name="Ewa" surname="Bit" />
             <MyName name="Ewa" />
-            <MyFavouriteDishes dishes={dishes} />
+            <MyFavouriteDishes dishes={dishes} /> */}
           </Route>
+
           <Route path="/game" element={<Game name="Szaszki"/>} >
-            
           </Route>
+
           <Route path="/users" element={<Users />} >
-            
           </Route>
+
           <Route path="/form" element={<Form />} >
-            
           </Route>
+
           <Route path="/contact" element={<Contact data={{
               tel: '4646345',
               address: 'Morenevegen 15',
               email: 'koteczki@tak.no'
             }} />} >
-            
           </Route>
+          
         </Routes>
       </Wrapper>
     </>
